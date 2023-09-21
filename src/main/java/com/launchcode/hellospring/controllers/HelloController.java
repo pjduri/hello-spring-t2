4e7fe9ba60ec9dbadec8f124942bebc1ff2d5777
Your Name <108307350+pjduri@users.noreply.github.com>
2023-09-20 22:58:11 -0500

    "path variables"

--- src/main/java/com/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/com/launchcode/hellospring/controllers/HelloController.java
@@ -1,7 +1,9 @@
 package com.launchcode.hellospring.controllers;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -25,6 +27,13 @@ public class HelloController {
     @GetMapping("hello")
     @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello " + name + "!";
+    }
+
+    // handles requests of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name) {
         return "Hello " + name + "!";
     }
 

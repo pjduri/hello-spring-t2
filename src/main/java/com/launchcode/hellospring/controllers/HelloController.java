b80bd5e3ebbe19524158794ef1338b7befacb698
Your Name <108307350+pjduri@users.noreply.github.com>
2023-10-01 23:04:48 -0500

    view with iteration

--- src/main/java/com/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/com/launchcode/hellospring/controllers/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 //@RequestMapping("hello")
 public class HelloController {
@@ -27,6 +30,18 @@ public class HelloController {
     @GetMapping("form")
     public String helloForm() {
         return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("Rocko");
+        names.add("Jocko");
+        names.add("Pocko");
+        names.add("Crocko");
+        model.addAttribute("names", names);
+
+        return "hello-list";
     }
 
 }
